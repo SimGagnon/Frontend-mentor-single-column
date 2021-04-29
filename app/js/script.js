d02a1acc1cd3c83b9cdec29b7c54eadcf6632fc7
Simon Gagnon <simongag93@gmail.com>
2021-04-29 14:46:45 -0400

    finished desktop version

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log("sup");
+const email = document.getElementById("email");
+const form = document.querySelector('form');
+
+
+
+form.addEventListener('submit' , function(e) {
+
+    const emailValue = email.value;
+    e.preventDefault();
+    
+    if (!validateEmail(emailValue)) {
+        form.classList.add('error');
+    } else {
+        form.classList.remove('error');
+    }
+})
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
